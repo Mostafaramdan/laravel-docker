9249d3eefcce0cf957737807e22c9b3dc7c10f98
mostafa ramdan <mostafaramdan5544@gmail.com>
2024-08-27 22:18:19 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome to laravel , reserved by nginx ';
+    return 'welcome to laravel , reserved by nginx.';
 });
